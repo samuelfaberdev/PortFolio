c09427dca598bbb2362678713b0621b572c9010d
Samuel Faber <samuel.faber@live.fr>
2024-10-16 21:22:26 +0200

    Ajout de la gestion des onglets

--- app/components/AboutSection.tsx
+++ app/components/AboutSection.tsx
@@ -1,7 +1,109 @@
+"use client";
+
+import Image from "next/image";
+import { ReactElement, useState, useTransition } from "react";
+
+interface DATA {
+  title: string;
+  id: string;
+  content: ReactElement;
+}
+const DATA: DATA[] = [
+  {
+    title: "Hard Skills",
+    id: "hardskills",
+    content: (
+      <ul className="list-disc pl-8">
+        <li>Front : React.js // Next.js // Tailwind</li>
+        <li>Back : Node.js // Express // GraphQL // TypeORM</li>
+        <li>Base de données : SQL // MySQL // PostgreSQL // SQLite</li>
+        <li>Veille technologique</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Soft Skills",
+    id: "softskills",
+    content: (
+      <ul className="list-disc pl-8">
+        <li>Travail en équipe</li>
+        <li>Résolution de problèmes</li>
+        <li>Gestion des priorités</li>
+        <li>Prise de décision rapide</li>
+        <li>Capacité d&apos;adaptation</li>
+        <li>Polyvalence, réactivité, autonomie</li>
+        <li>Aime être challengée</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-8">
+        <li>
+          Titre RNCP - Concepteur Développeur d'Applications -{" "}
+          <a href="https://wildcodeschool.fr/" className="gradient-text">
+            Wild Code School
+          </a>
+        </li>
+        <li>
+          Titre RNCP - Développeur Web et Web Mobile -{" "}
+          <a href="https://wildcodeschool.fr/" className="gradient-text">
+            Wild Code School
+          </a>
+        </li>
+        <li>
+          Licence Pro Ingénierie et Maintenance Biomédicales -{" "}
+          <a
+            href="http://www-iutlorient.univ-ubs.fr/"
+            className="gradient-text"
+          >
+            IUT de Lorient
+          </a>
+        </li>
+        <li>
+          DUT Génie Industriel et Maintenance -{" "}
+          <a href="https://iut-sn.univ-nantes.fr/" className="gradient-text">
+            IUT de Saint-Nazaire
+          </a>
+        </li>
+      </ul>
+    ),
+  },
+  {
+    title: "Experiences",
+    id: "experiences",
+    content: (
+      <ul className="list-disc pl-8">
+        <li>Teaching Assistant à la Wild Code School</li>
+      </ul>
+    ),
+  },
+];
+
 export default function AboutSection() {
+  const [tab, setTab] = useState("hardskills");
+
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id: string) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
   return (
-    <section className="w-full p-4 flex items-center justify-between gap-8">
-      <div className="w-2/6"></div>
+    <section className="w-full p-4 flex justify-between gap-32">
+      <div className="w-2/6 rounded-lg">
+        <Image
+          className="rounded-3xl"
+          src={"/images/bureau.jpg"}
+          alt="Un bureau d'ordinateur"
+          width={480}
+          height={480}
+        />
+      </div>
       <article className="flex flex-col gap-4 w-4/6">
         <h2 className="text-4xl">A propos de moi</h2>
         <p>
@@ -14,6 +116,30 @@ export default function AboutSection() {
           au fil de ma reconversion et de mon parcours professionnel, ainsi que
           mes différentes expériences dans le milieu du développement.
         </p>
+        <div className="flex gap-4">
+          <button onClick={() => handleTabChange("hardskills")}>
+            {" "}
+            Hard Skills <p className={tab === "hardskills" ? "active" : ""}></p>
+          </button>
+
+          <button onClick={() => handleTabChange("softskills")}>
+            {" "}
+            Soft Skills <p className={tab === "softskills" ? "active" : ""}></p>
+          </button>
+
+          <button onClick={() => handleTabChange("education")}>
+            {" "}
+            Education <p className={tab === "education" ? "active" : ""}></p>
+          </button>
+
+          {/* Bouton pour l'onglet "Experiences & Certifications" */}
+          <button onClick={() => handleTabChange("experiences")}>
+            {" "}
+            Experiences & Certifications{" "}
+            <p className={tab === "experiences" ? "active" : ""}></p>
+          </button>
+        </div>
+        <div>{DATA.find((t) => t.id === tab)?.content}</div>
       </article>
     </section>
   );
