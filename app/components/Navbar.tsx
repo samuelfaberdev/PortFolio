e152881de181b5bed431d03da5210d72115a2526
Samuel Faber <samuel.faber@live.fr>
2024-10-16 19:49:51 +0200

    ✨ Add Navbar component with logo and navigation links

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+export default function Navbar() {
+  return (
+    <header className="h-24 fixed top-0 p-4 flex items-center justify-between w-full border-b-2 border-neutral-800 bg-[var(--background)] opacity-80">
+      <span className="flex items-center justify-center gap-4 text-4xl">
+        <Image
+          src="/images/icone.svg"
+          alt="Logo"
+          className="h-14"
+          height={56}
+          width={56}
+        />
+        <p className="gradient-text">samuelfaber.dev</p>
+      </span>
+      <nav>
+        <ul className="flex text-xl gap-8">
+          <li>A propos</li>
+          <li>Mes projets</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
