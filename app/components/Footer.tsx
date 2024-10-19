dee077ad1d39bd870e8000cbe2d65ac1e7273200
Samuel Faber <samuel.faber@live.fr>
2024-10-19 15:15:06 +0200

    💄 Add Footer component with copyright information

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="p-8 flex items-center justify-center">
+      Copyright &copy; 2024 avec 💖 par Samuel Faber
+    </footer>
+  );
+}
