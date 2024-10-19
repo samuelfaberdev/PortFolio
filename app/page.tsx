10592253abf52139b8e790e97fd1b0c8a396f765
Samuel Faber <samuel.faber@live.fr>
2024-10-19 15:15:59 +0200

    ✨ Add ContactSection component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 import AboutSection from "./components/AboutSection";
+import ContactSection from "./components/ContactSection";
 import HeroSection from "./components/HeroSection";
-import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <>
-      <Navbar />
-      <main className="animate-fade-up flex flex-col gap-8">
-        <HeroSection />
-        <AboutSection />
-      </main>
-    </>
+    <main className="animate-fade-up flex flex-col gap-8">
+      <HeroSection />
+      <AboutSection />
+      <ContactSection />
+    </main>
   );
 }
