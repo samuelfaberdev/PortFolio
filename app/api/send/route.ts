ff7a93106b0437118a4440c413c20738feced035
Samuel Faber <samuel.faber@live.fr>
2024-10-30 18:22:12 +0100

    ✨ Mise à jour de l'API d'envoi d'e-mails : changement de l'expéditeur, ajout de BCC et amélioration de la gestion des messages

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -5,21 +5,21 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendToEmail = process.env.TO_EMAIL;
 
 export async function POST(req: Request, res: Response) {
-  const { email, subject, message } = await req.json();
-  console.log(email, subject, message);
   try {
+    const { email, subject, message } = await req.json();
     const data = await resend.emails.send({
-      from: "Contact - Samuel Faber <no-reply@samuelfaber.dev>",
-      to: [sendToEmail, email],
+      from: "Contact - Samuel Faber <contact@samuelfaber.dev>",
+      to: email,
+      bcc: sendToEmail,
       subject: subject,
       html: `
-        
         <p>Merci pour votre prise de contact, je vous réponds au plus vite !</p>
         <h1>Votre message :</h1>
         <h2>${subject}</h2>
         <p>${message}</p>
       `,
     });
+    console.info(res)
     return NextResponse.json(data);
   } catch (error) {
     return NextResponse.json({ error });
